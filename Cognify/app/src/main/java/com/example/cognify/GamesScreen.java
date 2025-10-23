d6c65bcc7e33407b09ba3b1611ff14ee1801dfdc
Nicholas22258 <nholeong@gmail.com>
2025-10-23 11:11:00 +0200

    Update GamesScreen.java

--- Cognify/app/src/main/java/com/example/cognify/GamesScreen.java
+++ Cognify/app/src/main/java/com/example/cognify/GamesScreen.java
@@ -55,7 +55,7 @@ public class GamesScreen extends AppCompatActivity {
         setContentView(R.layout.games_screen);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, 0);
             return insets;
         });
 //        TermsAndDefinitions.loadDummyTsAndDs();
